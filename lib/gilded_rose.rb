d57e40b0ee9199b6f0b46331018083d1b612e0a8
sanjsanj <sanjsanj@hotmail.com>
2015-08-16 14:38:38 +0100

    still 26/38 passing, refactor out lower_sell_in method and put logic into a ternary in lower_quality method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -8,16 +8,11 @@ class GildedRose
   def update_quality
     items.each do |item|
       item.sell_in -= 1
-      item.quality > 0 ? lower_quality(item) : nil
+      lower_quality item if item.quality > 0
     end
   end
 
   def lower_quality item
-    item.quality -= 1
-    item.sell_in < 0 ? lower_sell_in(item) : nil
-  end
-
-  def lower_sell_in item
-    item.quality -= 1
+    item.sell_in < 0 ? item.quality -= 2 : item.quality -= 1
   end
 end
