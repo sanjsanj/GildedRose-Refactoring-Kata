ab11f8e1c89bc3b4fd2b0b770a45f8d0eb1611db
sanjsanj <sanjsanj@hotmail.com>
2015-08-16 07:38:14 +0100

    separate item class, begin refactoring gilded rose class

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,16 +1,17 @@
-class GildedRose
+require 'item'
 
-  def initialize(items)
+class GildedRose
+  attr_reader :items
+
+  def initialize items
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
+  def update_quality
+    items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
+          item.quality -= 1
         end
       else
         if item.quality < 50
@@ -18,51 +19,31 @@ class GildedRose
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
             if item.sell_in < 6
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+
+      item.sell_in -= 1 if item.name != "Sulfuras, Hand of Ragnaros"
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
+            item.quality -= 1 if item.quality > 0
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          item.quality += 1 if item.quality < 50
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
